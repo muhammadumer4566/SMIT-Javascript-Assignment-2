20100b284179166449f775eefde8c255b0731624
muhammadumer4566 <muhammadumerfayyaz.2002@gmail.com>
2023-09-07 23:54:26 +0500

    JS

---
+++ app.js
@@ -0,0 +1,160 @@
+//      !   Chapter 05      (JS Assignment 2)
+
+// ? Q1)
+// var a=3;
+// var b=5;
+// var sum=a+b;
+// document.write("Sum of 3 and 5 is "+ sum );                
+
+//? Q2)
+// var a=3;
+// var b=5;
+// var sub=a-b;
+// document.write("Sub of 3 and 5 is "+ sub ); 
+
+// var a=3;
+// var b=5;
+// var mul=a*b;
+// document.write("Mul of 3 and 5 is "+ mul ); 
+
+// var a=3;
+// var b=5;
+// var div=a/b;
+// document.write("div of 3 and 5 is "+ div );
+
+// var a=3;
+// var b=5;
+// var mod=a%b;
+// document.write("Mod of 3 and 5 is "+ mod );
+
+//? Q3) part a,b
+
+// var value;
+// document.write("Value after variable declaration is  "+ value +"<br>");
+
+// ? part c,d
+
+// var value=5;
+// document.write("Initial value is "+ value +"<br>");
+
+// ? part e,f
+
+//   var a=5;
+//   var b=++a;
+//   document.write("Value after increment is "+ b +"<br>");
+
+// ? part g,h
+
+//   var a=5;
+//   var b=++a;
+//   var c=b+7;
+//   document.write("Value after addition is "+ c +"<br>");
+
+// ? part i,j
+
+// var a=5;
+// var b=++a;
+// var c=b+7;
+// var d=--c
+// document.write("Value after decrement is "+ d +"<br>");
+
+// ? part k,l
+
+// var a=5;
+// var b=++a;
+// var c=b+7;
+// var d=--c
+// var g=d%3;
+// document.write("The remainder is "+ g +"<br>");
+
+// ? Q4)
+
+// var oneTicket = 600;
+// var result = oneTicket*5;
+// document.write("Total cost to buy 5 tickets to a movie is "+ result +"PKR");
+
+// ? Q5)
+
+// var x = 4;
+// for(i=1 ; i<=10; i++){
+// document.write(`${x} X ${i} = ${x*i} <br/>`);
+//  }
+
+// ? Q6)   part a,b
+
+// var C= 25;
+// var cTof= (25*9/5)+32
+// document.write( C + 'C&deg;'+ "  " + "is" + " "+cTof +'F&deg;' +"<br>");
+
+// ? part c,d
+
+// var F = 70;
+// var fToc= (70-32)*5/9;
+// document.write( F + 'F&deg;' +  " " + "is"+" "+ fToc +'C&deg;' +"<br>");
+
+// ? Q7)
+
+// var PriceItem1=650;
+// document.write("Price of item 1 is "+PriceItem1 + "<br>");
+
+// var QuantityItem1=3;
+// document.write("Quantity of item 1 is "+QuantityItem1+ "<br>");
+
+// var PriceItem2=100;
+// document.write("Price of item 2 is "+PriceItem2 + "<br>");
+
+// var QuantityItem2=7;
+// document.write("Quantity of item 2 is "+QuantityItem2+ "<br>");
+
+// var charges=100;
+// document.write("Shipping Charges " +charges+"<br><br>");
+
+// var total=PriceItem1*QuantityItem1+PriceItem2*QuantityItem2+charges;
+// document.write("Total cost of your order is "+total);
+
+// ? Q8)
+
+// var a = 980;
+// document.write("Total Marks: " + a + "<br>");
+// var b = 804;
+// document.write("Obtained Marks: " + b + "<br>");
+// var result= (b/a)*100;
+// document.write("Percentage: "+result);
+
+// ? Q9)
+
+// var usDoller=10;
+// var SaudiRiyal=25;
+// var inPKR= usDoller* 305.80 + SaudiRiyal * 84;
+// document.write("Total currency in PKR "+inPKR);
+
+// ? Q10)
+
+// var a=7;
+// var b= a+5*10/2;
+// document.write("Result: "+b);   
+
+// ? Q11)
+
+// var currentYear = 2016;
+// var birthYear = 1992;
+// var age = currentYear - birthYear;
+// document.write("Your age is: " + age);
+
+// ? Q12)
+
+// var radius = 20;
+// document.write("Radius of a circle:" + radius +"<br/>");
+// var cir = 2 *3.142*20;
+// document.write("The circumference is :" + cir +"<br/>");
+// var area = 3.142*20*20;
+// document.write("The area is :"+area);
+
+// ? Q13)
+
+// var snack = "chocolate chip";
+// var currentAge = 15;
+// var MaxAge = 65;
+// var AmountSnack = 3;
+// var totalSnack = 50 * 3;
+// document.write("You will need" + totalSnack + "chocolate chip to last you until the ripe old age of" + MaxAge);
